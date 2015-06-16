ea230afa8df3ccccc99220c0e10fe83c0da09a40
charudath <cgowdas@gmail.com>
2015-06-16 13:53:05 +0530

    initial after recut

---
+++ src/test/java/com/charu/roorest/domain/UserIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.charu.roorest.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = User.class)
+public class UserIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
